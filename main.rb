41bc4c9a0d1b465b2c012d865ef6a222d3279c40
junpei.nishina <junpei.nishina@bizreach.co.jp>
2016-01-07 17:33:38 +0900

    定期的にファイルをS3アップロード

--- main.rb
+++ main.rb
@@ -6,20 +6,37 @@ require 'aws-sdk'
 logger = Logger.new(STDOUT)
 
 # AWSクライアントセットアップ
-s3 = Aws::S3::Client.new
+@s3 = Aws::S3::Client.new
 
-logger.debug(s3.list_buckets)
+logger.debug(@s3.list_buckets)
 
 # 最新の画像取得
-def get_latest_image()
+def get_latest_image_path()
   file_path = Dir.glob("/tmp/motion/*.jpg").max_by {|f| File.mtime(f)}
+end
+
+# S3へのファイルアップロード
+def upload_file_to_aws(file_path)
+  file = File.open(file_path)
+  file_name = File.basename(file_path)
+  
+  @s3.put_object(
+    bucket: "biz-hackathon",
+    body: file,
+    key: file_name
+  )
 end
 
 # タスクの定期実行用
 EM.run do
-  # 10秒ごとにタスクを実行する
-  EM::PeriodicTimer.new(10) do
+  # 定期的にタスクを実行する
+  EM::PeriodicTimer.new(30) do
     logger.info("trigger")
-    logger.info(get_latest_image())
+    
+    file_path = get_latest_image_path()
+    logger.info(file_path)
+    
+    upload_file_to_aws(file_path)
+    logger.info("finished file upload")
   end
 end
