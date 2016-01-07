d647ea1c1d90ed20c2fc4649bf7886db84d1e063
junpei.nishina <junpei.nishina@bizreach.co.jp>
2016-01-07 23:07:56 +0900

    get pin value from gpio18

--- main.rb
+++ main.rb
@@ -27,16 +27,26 @@ def upload_file_to_aws(file_path)
   )
 end
 
+# pinの値を取得して返す。
+def get_pin_value()
+  pin = File.open("/sys/class/gpio/gpio18/value")
+  value = pin.read()[0].to_i
+  pin.close
+  value
+end
+
 # タスクの定期実行用
 EM.run do
   # 定期的にタスクを実行する
-  EM::PeriodicTimer.new(30) do
-    logger.info("trigger")
-    
+  EM::PeriodicTimer.new(5) do
     file_path = get_latest_image_path()
-    logger.info(file_path)
+    logger.info("file_path: #{file_path}")
     
-    upload_file_to_aws(file_path)
-    logger.info("finished file upload")
+    pin_value = get_pin_value()
+    logger.debug("pin value: #{pin_value}")
+    if pin_value == 1 then
+      upload_file_to_aws(file_path)
+      logger.info("finished file upload")
+    end
   end
 end
